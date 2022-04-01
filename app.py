575aa9c0d92fe08c88fa8e25b16ce96b0d8b3de3
pedro <pedro@LAPTOP-6E3V8EEC.mshome.net>
2022-04-01 11:23:09 -0300

    Adding Todo to database

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template,url_for
+from flask import Flask, render_template,url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
@@ -14,9 +15,21 @@ class Todo(db.Model):
         return '<Task %r>' % self.id
 
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    return render_template('index.html')
+    if request.method=='POST':
+        task_content = request.form['content']
+        new_task=Todo(content=task_content)
+        
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'Error adding task'
+    else:
+        tasks=Todo.query.order_by(Todo.date_created).all()
+        return render_template('index.html', tasks=tasks)
 
 if __name__ == "__main__":
     app.run(debug=True)
